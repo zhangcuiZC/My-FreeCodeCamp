82515a7095e197d8b93eb6914306fc149fa2d7a1
ZZZZcccc <zczhangcui@foxmail.com>
2017-01-06 14:20:21 +0800

    complete twitch api project

--- twitch/js/main.js
+++ twitch/js/main.js
@@ -1,20 +1,48 @@
 $(function(){
 	(function(){
-		$.ajax({
-			url: 'https://api.twitch.tv/kraken/streams/summary',
-			headers:{
-				Accept: 'application/javascript',
-				'Client-Id':'1gpgz9i8q62g3jqrpr3csg8e4sj7u9'
-			},
-			success:function(response){
-				console.log(response);
-			}
+		var width=$(".content").width();
+		function search(){
+			$.ajax({
+				url: 'https://api.twitch.tv/kraken/streams?game=dota%202&limit=16',
+				headers:{
+					Accept: 'application/vnd.twitchtv.v3+json',
+					'Client-Id':'1gpgz9i8q62g3jqrpr3csg8e4sj7u9'
+				},
+				success:function(response){
+					$(".content .item").each(function(index) {
+						var info=response.streams[index];
+						$(this).attr('href', info.channel.url);	
+						$(this).find('img').attr('src', info.preview.medium).css('height',0.5625*0.24*width);
+						$(this).find('.status').text(info.channel.status);
+						$(this).find('.name').text(info.channel.display_name);
+						$(this).find('.num span').text(info.viewers);
+					});
+
+				}
+			});
+
+			$(".topbar").animate({marginTop:"10px"}, 400, function(){
+				$(".content").animate({height:"show"}, 400);
+			});
+		}
+
+		$(".btn").click(function(event) {
+			search();
 		});
-		
 
+		var timer=null;
+		$(window).resize(function(event) {
+			timer=setTimeout(function(){
+				clearTimeout(timer);
+				var width=$(".content").width();
+				$(".item img").css('height',0.5625*0.24*width);
+			},200);
+		});
 
-
-
-
+		$(".item").hover(function() {
+			$(this).find('.layer').fadeIn(200);
+		}, function() {
+			$(this).find('.layer').fadeOut(200);
+		});
 	})();
 });
