9cc0d348cb53d0b693f795f24cc35599c2542bc0
ZC <zczhangcui@foxmail.com>
2017-02-16 22:15:44 +0800

    完善AI

--- TicTacToe/js/main.js
+++ TicTacToe/js/main.js
@@ -1,10 +1,13 @@
 $(function(){
 	var $item=$(".ttt-item"),
-		playerturn=true,
 		playerStyle="O",
-		gameover=false;
-		computerStyle="X";
+		computerStyle="X",
+		playerturn=true,
+		gameover=false,
+		winmsg="",
+		i;
 
+	// 绑定事件，表明玩家可以开始了
 	$item.click(function(event) {
 		if(playerturn){
 			playerRound(event);
@@ -16,8 +19,12 @@ $(function(){
 		if($el.text()===""){
 			playerturn=false;
 			$el.text(playerStyle).addClass('player');
-			checkWinner();
-			computerRound();
+			winmsg=checkWinner();
+			if(winmsg){
+				gameOver(winmsg);
+			}else{
+				computerRound();
+			}
 		}
 	}
 
@@ -28,12 +35,45 @@ $(function(){
 				return $item.eq(index).text()==="";
 			});
 			if($emptyitem.length===0){
-				return;
+				$("h1").text("和局");
+				setTimeout(function(){
+					reset();
+				}, 1000);
 			}
 
-			var idx=Math.floor(Math.random()*$emptyitem.length);
-			$emptyitem.eq(idx).text(computerStyle).addClass('ai');
-			checkWinner();
+			// 寻找电脑能赢的方法
+			for(i=0;i<$emptyitem.length;i++){
+				$emptyitem.eq(i).text(computerStyle).addClass('temp');
+				winmsg=checkWinner();
+				if(winmsg){
+					$emptyitem.eq(i).addClass('ai').removeClass('temp');
+					gameOver(winmsg);
+					return;
+				}else{
+					$emptyitem.eq(i).text("").removeClass('temp');
+				}
+			}
+
+			// 如果电脑无法赢，寻找不让玩家赢的方法
+			for(i=0;i<$emptyitem.length;i++){
+				$emptyitem.eq(i).text(playerStyle).addClass('temp');
+				winmsg=checkWinner();
+				if(winmsg){
+					$emptyitem.eq(i).text(computerStyle).addClass('ai').removeClass('temp');
+					playerturn=true;
+					return;
+				}else{
+					$emptyitem.eq(i).text("").removeClass('temp');
+				}
+			}
+
+			// 啥都没找到，瞎比下一个吧_(:з)∠)_
+			if($item.eq(4).text()===""){
+				$item.eq(4).text(computerStyle).addClass('ai');
+			}else{
+				var idx=Math.floor(Math.random()*$emptyitem.length);
+				$emptyitem.eq(idx).text(computerStyle).addClass('ai');
+			}
 			playerturn=true;
 		}
 	}
@@ -45,41 +85,52 @@ $(function(){
 		}
 		if(textarr[0]!==""&&textarr[0]===textarr[1]&&textarr[1]===textarr[2]){
 			msg=textarr[0]+" is the winner!";
-			gameOver(msg);
+			return msg;
 		}
 		if(textarr[3]!==""&&textarr[3]===textarr[4]&&textarr[4]===textarr[5]){
 			msg=textarr[3]+" is the winner!";
-			gameOver(msg);
+			return msg;
 		}
 		if(textarr[6]!==""&&textarr[6]===textarr[7]&&textarr[7]===textarr[8]){
 			msg=textarr[6]+" is the winner!";
-			gameOver(msg);
+			return msg;
 		}
 		if(textarr[0]!==""&&textarr[0]===textarr[3]&&textarr[3]===textarr[6]){
 			msg=textarr[0]+" is the winner!";
-			gameOver(msg);
+			return msg;
 		}
 		if(textarr[1]!==""&&textarr[1]===textarr[4]&&textarr[4]===textarr[7]){
 			msg=textarr[1]+" is the winner!";
-			gameOver(msg);
+			return msg;
 		}
 		if(textarr[2]!==""&&textarr[2]===textarr[5]&&textarr[5]===textarr[8]){
 			msg=textarr[2]+" is the winner!";
-			gameOver(msg);
+			return msg;
 		}
 		if(textarr[0]!==""&&textarr[0]===textarr[4]&&textarr[4]===textarr[8]){
 			msg=textarr[0]+" is the winner!";
-			gameOver(msg);
+			return msg;
 		}
 		if(textarr[2]!==""&&textarr[2]===textarr[4]&&textarr[4]===textarr[6]){
 			msg=textarr[2]+" is the winner!";
-			gameOver(msg);
+			return msg;
 		}
+		return msg;
 	}
 
 	function gameOver(msg){
 		gameover=true;
 		playerturn=false;
 		$("h1").text(msg);
+		setTimeout(function(){
+			reset();
+		}, 1000);
+	}
+
+	function reset(){
+		$item.text("").removeClass('ai player temp');
+		gameover=false;
+		playerturn=true;
+		$("h1").text("");
 	}
 });
